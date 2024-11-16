4b7c56fd17dc83aa68c6cbb19b558954d4838c92
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-16 05:58:49 +0100

    tablet

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const iconMenuElement = document.getElementById("icon-menu")
+const menuElement = document.getElementById("menu")
+
+
+const toggleMenu = () => {
+    menuElement.classList.toggle("menu-show")
+}
+
+
+iconMenuElement.addEventListener("click", toggleMenu)
