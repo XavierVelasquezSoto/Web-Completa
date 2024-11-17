04264f0edae5e9c6f720a1734a1b320c6d8b5595
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-17 02:24:20 +0100

    about

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,17 @@ const menuElement = document.getElementById("menu")
 
 const toggleMenu = () => {
     menuElement.classList.toggle("menu-show")
-}
 
+    if ( iconMenuElement.src.includes("hamburger.svg") )
+        {
+            iconMenuElement.src = `./assets/img/icons/close.svg`
+            iconMenuElement.src = `../assets/img/icons/close.svg`
+        }
+    else
+        {
+            iconMenuElement.src = `./assets/img/icons/hamburger.svg`
+            iconMenuElement.src = `../assets/img/icons/hamburger.svg`
+        }
+}
 
 iconMenuElement.addEventListener("click", toggleMenu)
