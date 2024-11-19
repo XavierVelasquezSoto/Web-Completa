2c6d50e41099497db3c32569ccf3547c4a390476
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-20 00:01:25 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ const toggleMenu = () => {
 
     if ( iconMenuElement.src.includes("hamburger.svg") )
         {
-            iconMenuElement.src = `./assets/img/icons/close.svg`
+            /* iconMenuElement.src = `./assets/img/icons/close.svg` */
             iconMenuElement.src = `../assets/img/icons/close.svg`
         }
     else
         {
-            iconMenuElement.src = `./assets/img/icons/hamburger.svg`
+           /*  iconMenuElement.src = `./assets/img/icons/hamburger.svg` */
             iconMenuElement.src = `../assets/img/icons/hamburger.svg`
         }
 }
@@ -21,7 +21,7 @@ iconMenuElement.addEventListener("click", toggleMenu)
 
 
 
-const accordionElement = document.getElementById('faq-container');
+/* const accordionElement = document.getElementById('faq-hidden');
 const allAccordionTexts = document.querySelectorAll('.faq-text');
 
 const toggleAccordionText = event => {
@@ -30,7 +30,19 @@ const toggleAccordionText = event => {
   //   accordionText.classList.remove('accordion-text-show');
   // }
 
-  event.target.children[0].children[1].classList.toggle('faq-text-show');
+  event.target.children[0].children[1].classList.toggle('faq-hidden-show');
+};
+
+accordionElement.addEventListener('click', toggleAccordionText); */
+
+
+const accordionElement = document.getElementById('faq-hidden');
+
+const toggleAccordionText = event => {
+  event.target.classList.toggle('faq-hidden-show');
+  console.log(accordionElement.classList);
 };
 
 accordionElement.addEventListener('click', toggleAccordionText);
+
+
